5f503eec74ad26fab69506e5049c5396c95c4d07
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-02 21:03:56 +0530

    Add files via upload

---
+++ a2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the intrger: ");
+    scanf("%d", &num);
+    printf("You entered: %d\n", num);
+    return 0;
+}
+
+//Read an integer from the user and print it

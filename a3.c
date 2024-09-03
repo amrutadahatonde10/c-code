627234f9dbdb8711162af4822aeecbb00f2db7b4
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-03 17:50:49 +0530

    Add files via upload

---
+++ a3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the 1st integer: ");
+    scanf("%d", &a);
+    printf("Enter the 2nd integer: ");
+    scanf("%d", &b);
+    printf("You entered: %d and %d\n" , a,b);
+    return 0;
+}
+
+//Read two integers from the user and print them

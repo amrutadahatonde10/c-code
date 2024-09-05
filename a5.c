95a3037159b30206e1e75f733131c3912b8f4167
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-05 17:33:32 +0530

    Add files via upload

---
+++ a5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    int b=10;
+    int c;
+    printf("a=%d,b=%d,c=%d",a,b,c);
+    c=b;
+    b=a;
+    a=c;
+    printf("a=%d,b=%d,c=%d",a,b,c);
+    return 0;
+}
+
+//swap two numbers using a third variable

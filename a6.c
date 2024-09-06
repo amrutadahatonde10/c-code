4176e054414746a5261b23d774b76d0e2cc0e519
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-06 19:35:06 +0530

    Add files via upload

---
+++ a6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    printf("Enter an integer i,j =");
+    scanf("%d , %d", &i , &j);
+    printf("i=%d",i);
+    return 0;
+}
+
+//swap two numbers without using third variable.

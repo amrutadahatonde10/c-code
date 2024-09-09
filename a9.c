d65b47a47e02b4d4068711793d12846d7cad2bd3
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-09 18:23:14 +0530

    Add files via upload

---
+++ a9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char character; 
+
+    character = 'A';
+
+    printf("The character is: %c\n", character);
+
+    return 0;
+}
+
+//Program to assign a character to a variable and print it.

4559bdba16e69e91a01cfb47a33f2e6f2ecfe2a0
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-10 22:07:43 +0530

    Add files via upload

---
+++ a10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    printf("Enter the charcter: ");
+    scanf(" %c", &ch);
+
+    printf("You entered : %c\n", ch);
+}
+
+//Program to read a character from a user, assign it to a variable and print it.

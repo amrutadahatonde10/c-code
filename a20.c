77fdf94e394c11011fc7ba60aac42ab396dc0562
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-20 18:18:59 +0530

    Add files via upload

---
+++ a20.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("Enter the marks: ");
+    scanf("%d", &marks);
+    if(marks >= 0 && marks <= 35){
+        printf("Fail \n");
+    }
+    else if(marks > 35 && marks <= 100){
+        printf("Pass \n");
+    }
+    else {
+        printf("Wrong Marks");
+    }
+    return 0;
+}
+
+//write a program to check if a student passed or failed.

004faec9287f84f2e4f44721133b8a4bc8d24dc7
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-09-21 16:29:43 +0530

    Add files via upload

---
+++ a21.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    for(int i=1; i<=10; i++){
+        if(i == 6) {
+            continue;
+        }
+        printf("%d \n", i);
+    }
+    return 0;
+}
+
+//print all numbers from 1 to 10 except for 6.

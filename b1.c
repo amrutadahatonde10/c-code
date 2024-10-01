c01e6505c095cdf1151d799f33f6feced8b31285
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-10-01 17:50:01 +0530

    Add files via upload

---
+++ b1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int rows = 5;
+
+    for (int i = 0; i < rows; i++){
+          for (int j = rows; j > i; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+// Write a c program to print this pattern using nested loop
+// *****
+// ****
+// ***
+// **
+// *

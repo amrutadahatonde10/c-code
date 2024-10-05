fd322e5b9b12f71b0d9bd33ae246a27ea7aea681
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-10-05 16:53:15 +0530

    Add files via upload

---
+++ b5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int num , sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num; i++){
+       if (i % 2 == 0){
+        sum += i;
+       } 
+    }
+
+    printf("The sum of even numbers up to %d is: %d\n", num, sum);
+
+    return 0;
+}
+
+//write a c program to calculate the sum of even numbers up to N

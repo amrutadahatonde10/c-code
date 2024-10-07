f54d5be329daa061f0db2d7a4b136df82f82e73e
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-10-07 17:18:53 +0530

    Add files via upload

---
+++ b7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char ch;
+
+    for (ch = 'A'; ch <= 'Z'; ch++){
+        printf("%c", ch);
+    }
+    printf("\n");
+    return 0;
+}
+
+//write a c program to Print characters from A to Z using for loop.

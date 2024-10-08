73bfa9ff4e07e2ae9e85e002736d3cf4138e9060
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-10-08 11:48:21 +0530

    Add files via upload

---
+++ b8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++){
+        printf("%c", ch);
+    }
+    printf("\n");
+    return 0;
+}
+
+//write a c program to Print characters from a to z using for loop.

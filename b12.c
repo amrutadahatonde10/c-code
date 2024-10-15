d685fa46221fba354980287057b5fd2eee8244c3
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-10-15 18:10:23 +0530

    Add files via upload

---
+++ b12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float f;
+
+    printf("Enter number:");
+    scanf("%f", &f);
+
+    printf("Floating-point number is %.2f",f);
+
+    return 0;
+}
+
+//create a program that reads a floating-point number from user and prints with two decimal points.

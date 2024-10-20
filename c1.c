e39b18f508ef8c6a359558f118f448c9273440db
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2024-10-20 15:27:35 +0530

    Add files via upload

---
+++ c1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int N = 15;
+    int i = N;
+
+    while(i>0){
+        printf("%d\n",i);
+        i--;
+    }
+
+    return 0;
+}
+
+//count down from n to 1 using while loop

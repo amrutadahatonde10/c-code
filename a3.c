e8ad9971bb8b1d48450558cafbfeb781fe716fc8
Amruta Dahatonde <amrutadahatonde10@gmail.com>
2025-05-15 08:29:25 +0530

    Update a3.c

--- a3.c
+++ a3.c
@@ -2,7 +2,7 @@
 int main()
 {
     int a,b;
-    printf("Enter the 1st integer: ");
+    printf("Enter the 1st integer: "); 
     scanf("%d", &a);
     printf("Enter the 2nd integer: ");
     scanf("%d", &b);
